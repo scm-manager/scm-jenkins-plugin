5407cd06ea7d19f1c90e08c5c7bb6bed6cb1763d
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2011-11-18 15:04:14 +0100

    added comments for tutorial

--- src/main/java/sonia/scm/jenkins/JenkinsHook.java
+++ src/main/java/sonia/scm/jenkins/JenkinsHook.java
@@ -61,37 +61,53 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Jenkins post receive Hook.
+ * This class is called after a changeset successfully pushed to a repository.
+ * The jenkins hook checks if the hook is configured for this repository and
+ * calls the configured jenkins ci server if a valid configuration is a
+ * available. This class is a singleton, so be carefully.
  *
+ * Extension objects in SCM-Manager:
+ * https://bitbucket.org/sdorra/scm-manager/wiki/ExtensionPoints
+ * 
  * @author Sebastian Sdorra
  */
 @Extension
 public class JenkinsHook implements RepositoryHook
 {
 
-  /** Field description */
+  /** Http parameter for jenkins authentication token */
   public static final String PARAMETER_TOKEN = "token";
 
-  /** Field description */
+  /** Repository property for a comma seperated list of project keys */
   public static final String PROPERTY_JENKINS_PROJECT = "jenkins.project";
 
-  /** Field description */
+  /** Repository property for the jenkins authentication token */
   public static final String PROPERTY_JENKINS_TOKEN = "jenkins.token";
 
-  /** Field description */
+  /** Repository property for the jenkins ci server url */
   public static final String PROPERTY_JENKINS_URL = "jenkins.url";
 
   /** the logger for JenkinsHook */
   private static final Logger logger =
     LoggerFactory.getLogger(JenkinsHook.class);
 
+  /** The type(s) of the repository hooks. */
+  public static final Collection<RepositoryHookType> TYPES =
+    Arrays.asList(RepositoryHookType.POST_RECEIVE);
+
   //~--- constructors ---------------------------------------------------------
 
   /**
-   * Constructs ...
+   * Creates a new instance of Jenkins Hook. This constructor is called by
+   * Google Guice.Guice injects all necessary dependencies to the constructor.
+   * For more informations on Google Guice contructor injection have a look
+   * at http://code.google.com/p/google-guice/wiki/Injections.
+   * 
+   * Available objects for injection in SCM-Manager:
+   * https://bitbucket.org/sdorra/scm-manager/wiki/injectionObjects
    *
-   *
-   *
-   * @param httpClientProvider
+   * @param httpClientProvider Google Guice provider for an {@link HttpClient}
    */
   @Inject
   public JenkinsHook(Provider<HttpClient> httpClientProvider)
@@ -102,16 +118,24 @@ public class JenkinsHook implements RepositoryHook
   //~--- methods --------------------------------------------------------------
 
   /**
-   * Method description
+   * This method is called after a changeset is successfully pushed to a
+   * repository.
    *
    *
-   * @param event
+   * @param event the event of the hook. This object contains the changed
+   *        repository and the changesets.
    */
   @Override
   public void onEvent(RepositoryHookEvent event)
   {
+
+    // get the changed repository
     Repository repository = event.getRepository();
 
+    /** 
+     * check if the repository is not null. If the repository is null
+     * log an error.
+     */
     if (repository != null)
     {
       handleRepositoryEvent(repository);
@@ -125,22 +149,24 @@ public class JenkinsHook implements RepositoryHook
   //~--- get methods ----------------------------------------------------------
 
   /**
-   *   Method description
+   * Returns the types of the hook.
    *
    *
-   *   @return
+   * @return a {@link Collection} of repository types.
    */
   @Override
   public Collection<RepositoryHookType> getTypes()
   {
-    return Arrays.asList(RepositoryHookType.POST_RECEIVE);
+    return TYPES;
   }
 
   /**
-   * Method description
+   * Return true if the hook can be executed asynchronous.
+   * Note you can not access the current servlet or user if the hook is executed
+   * asynchronous.
    *
    *
-   * @return
+   * @return true if the hook is executed asynchronous.
    */
   @Override
   public boolean isAsync()
@@ -151,12 +177,12 @@ public class JenkinsHook implements RepositoryHook
   //~--- methods --------------------------------------------------------------
 
   /**
+   * Creates the url to the remote trigger servlet of jenkins.
    *
+   * @param url base url to the jenkins server
+   * @param project jenkins project name
    *
-   * @param url
-   * @param project
-   *
-   * @return
+   * @return the url to the remote trigger servlet of jenkins
    */
   private String createUrl(String url, String project)
   {
@@ -169,31 +195,45 @@ public class JenkinsHook implements RepositoryHook
   }
 
   /**
-   * Method description
+   * Handles the repository event. Checks the configuration for the repository
+   * and calls the jenkins server.
    *
    *
-   * @param repository
+   * @param repository which received the changesets
    */
   private void handleRepositoryEvent(Repository repository)
   {
+
+    // read the url property from the repository
     String url = repository.getProperty(PROPERTY_JENKINS_URL);
+
+    // read the project property from the repository
     String project = repository.getProperty(PROPERTY_JENKINS_PROJECT);
 
+    // check if both properties are not null and the length is greate then 0.
     if (Util.isNotEmpty(url) && Util.isNotEmpty(project))
     {
+
+      // create the url to the endpoint
       url = createUrl(url, project);
 
+      // log the url
       if (logger.isInfoEnabled())
       {
         logger.info("call jenkins at {}", url);
       }
 
+      // read the token property from the repository
       String token = repository.getProperty(PROPERTY_JENKINS_TOKEN);
 
       try
       {
+
+        // send the request to the jenkins server
         sendRequest(url, token);
       }
+
+      // catch each IOException and log them
       catch (IOException ex)
       {
         logger.error("could not send request to jenkins", ex);
@@ -202,34 +242,47 @@ public class JenkinsHook implements RepositoryHook
   }
 
   /**
-   * Method description
+   * Send the request to the jenkins ci server to trigger a new build.
    *
    *
-   * @param url
-   * @param token
+   * @param url url of the jenkins server
+   * @param token the authentication token of the jenkins project
    *
    * @throws IOException
    */
   private void sendRequest(String url, String token) throws IOException
   {
     HttpResponse response = null;
+
+    /**
+     * Create a new http client from the Guice Provider.
+     */
     HttpClient httpClient = httpClientProvider.get();
 
+    // check if the token is not empty.
     if (Util.isNotEmpty(token))
     {
+
+      // add the token as parameter for the request
       Map<String, List<String>> parameters = new HashMap<String,
                                                List<String>>();
 
       parameters.put(PARAMETER_TOKEN, Arrays.asList(token));
+
+      // execute the http post request with the http client
       response = httpClient.post(url, parameters);
     }
     else
     {
+
+      // execute the http post request with the http client
       response = httpClient.post(url);
     }
 
+    // fetch the status code of the response
     int sc = response.getStatusCode();
 
+    // if the response is greater than 400 write an error to the log
     if (sc >= 400)
     {
       logger.error("jenkins returned status code {}", sc);
@@ -238,6 +291,6 @@ public class JenkinsHook implements RepositoryHook
 
   //~--- fields ---------------------------------------------------------------
 
-  /** Field description */
+  /** Guice provider for {@link HttpClient} */
   private Provider<HttpClient> httpClientProvider;
 }
--- src/main/resources/sonia/scm/sonia.jenkins.js
+++ src/main/resources/sonia/scm/sonia.jenkins.js
@@ -29,35 +29,78 @@
  * 
  */
 
-
+/** 
+ * Register extjs namespace for the plugin.
+ * http://docs.sencha.com/ext-js/3-4/#!/api/Ext-method-ns
+ */
 Ext.ns('Sonia.jenkins');
 
+/**
+ * Create new component which extends the Sonia.repository.PropertiesFormPanel.
+ * This panel should be visible as tab at the bottom of the repository overview
+ * when a repository is selected and the current user is the admin of this 
+ * repository. This panel display a formular for the configuration of the 
+ * jenkins plugin. The setting of this formular is stored on the selected 
+ * repository.
+ */
 Sonia.jenkins.ConfigPanel = Ext.extend(Sonia.repository.PropertiesFormPanel, {
   
+  /**
+   * Labels and texts are stored as class properties
+   * for localisation.
+   */
+  
+  // title of config tab
   formTitleText: 'Jenkins',
+  // label of the url field
   urlText: 'Url',
+  // label of the project field.
   projectText: 'Project',
+  // label of the token field
   tokenText: 'Token',
   
+  // help text for the url field
   urlHelpText: 'Url of Jenkins installation (with contextpath).',
+  // help text for the project field
   projectHelpText: 'The name of the Jenkins project.',
+  // help text for the token field
   tokenHelpText: 'Jenkins Authentication Token',
   
-  initComponent: function(){    
+  /**
+   * This method initializes the component and configures it. 
+   */
+  initComponent: function(){
+    
+    /**
+     * Config object to configure the panel and
+     * its children.
+     */
     var config = {
+      // Title of the config panel.
       title: this.formTitleText,
+      // formular elements
       items: [{
+        // name of the field
         name: 'jenkinsUrl',
+        // label of the field
         fieldLabel: this.urlText,
+        /**
+         * Mapped property of the field. This is the key of the property stored
+         * on the repository object.
+         */
         property: 'jenkins.url',
+        // validation of the field
         vtype: 'url',
+        // help text
         helpText: this.urlHelpText
       },{
+        // jenkins project field
         name: 'jenkinsProject',
         fieldLabel: this.projectText,
         property: 'jenkins.project',
         helpText: this.projectHelpText
       },{
+        // jenkins token field
         name: 'jenkinsToken',
         fieldLabel: this.tokenText,
         property: 'jenkins.token',
@@ -65,20 +108,50 @@ Sonia.jenkins.ConfigPanel = Ext.extend(Sonia.repository.PropertiesFormPanel, {
       }]
     };
     
+    /**
+     * The apply method merges the initialConfig object with the config object.
+     * The initialConfig object is the config object from the parent panel 
+     * (in this case Sonia.repository.PropertiesFormPanel).
+     * http://docs.sencha.com/ext-js/3-4/#!/api/Ext-method-apply
+     */
     Ext.apply(this, Ext.apply(this.initialConfig, config));
+    
+    /**
+     * Call the initComponent method of the parent panel 
+     * (Sonia.repository.PropertiesFormPanel)).
+     */
     Sonia.jenkins.ConfigPanel.superclass.initComponent.apply(this, arguments);
   }
   
 });
 
-// register xtype
+/**
+ * Register xtype of the panel for later use and lazy initialization.
+ * http://docs.sencha.com/ext-js/3-4/#!/api/Ext-method-reg
+ */
 Ext.reg("jenkinsConfigPanel", Sonia.jenkins.ConfigPanel);
 
-// register panel
+/**
+ * Register a listener which is called, after repository is selected in the 
+ * web interface. The listener becomes the selected repository and an array
+ * of panels as argument.
+ */ 
 Sonia.repository.openListeners.push(function(repository, panels){
+  
+  /**
+   * check if the current user is the owner of the repository
+   */
   if (Sonia.repository.isOwner(repository)){
+    /**
+     * Append the jenkins config panel to the panels array.
+     */
     panels.push({
+      // registerd xtype for the panel
       xtype: 'jenkinsConfigPanel',
+      /**
+       * Selected repository. This parameter is required by the parent panel, 
+       * Sonia.repository.PropertiesFormPanel.
+       */
       item: repository
     });
   }
