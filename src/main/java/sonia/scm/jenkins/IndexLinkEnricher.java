06a302fec9f11bd831c93671aef73d94f2491674
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2025-02-19 11:40:06 +0100

    Migrate from json to hal enricher
    Squash commits of branch feature/enricher:
    
    - Migrate from json to hal enricher

--- src/main/java/sonia/scm/jenkins/IndexLinkEnricher.java
+++ src/main/java/sonia/scm/jenkins/IndexLinkEnricher.java
@@ -16,44 +16,39 @@
 
 package sonia.scm.jenkins;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.inject.Inject;
+import jakarta.inject.Provider;
+import sonia.scm.api.v2.resources.Enrich;
+import sonia.scm.api.v2.resources.HalAppender;
+import sonia.scm.api.v2.resources.HalEnricher;
+import sonia.scm.api.v2.resources.HalEnricherContext;
+import sonia.scm.api.v2.resources.Index;
 import sonia.scm.api.v2.resources.LinkBuilder;
 import sonia.scm.api.v2.resources.ScmPathInfoStore;
 import sonia.scm.config.ConfigurationPermissions;
 import sonia.scm.plugin.Extension;
-import sonia.scm.web.JsonEnricherBase;
-import sonia.scm.web.JsonEnricherContext;
 
-import jakarta.inject.Inject;
-import jakarta.inject.Provider;
-
-import static java.util.Collections.singletonMap;
 import static sonia.scm.jenkins.JenkinsContext.NAME;
-import static sonia.scm.web.VndMediaType.INDEX;
 
 @Extension
-public class IndexLinkEnricher extends JsonEnricherBase {
+@Enrich(Index.class)
+public class IndexLinkEnricher implements HalEnricher {
 
   private final Provider<ScmPathInfoStore> scmPathInfoStore;
 
   @Inject
-  public IndexLinkEnricher(Provider<ScmPathInfoStore> scmPathInfoStore, ObjectMapper objectMapper) {
-    super(objectMapper);
+  public IndexLinkEnricher(Provider<ScmPathInfoStore> scmPathInfoStore) {
     this.scmPathInfoStore = scmPathInfoStore;
   }
 
   @Override
-  public void enrich(JsonEnricherContext context) {
-    if (resultHasMediaType(INDEX, context) && ConfigurationPermissions.read(NAME).isPermitted()) {
+  public void enrich(HalEnricherContext halEnricherContext, HalAppender halAppender) {
+    if (ConfigurationPermissions.read(NAME).isPermitted()) {
       String globalJenkinsConfigUrl = new LinkBuilder(scmPathInfoStore.get().get(), JenkinsConfigurationResource.class)
         .method("getGlobalJenkinsConfig")
         .parameters()
         .href();
-
-      JsonNode hgConfigRefNode = createObject(singletonMap("href", value(globalJenkinsConfigUrl)));
-
-      addPropertyNode(context.getResponseEntity().get("_links"), "jenkinsConfig", hgConfigRefNode);
+      halAppender.appendLink("jenkinsConfig", globalJenkinsConfigUrl);
     }
   }
 }
--- src/main/java/sonia/scm/jenkins/RepositoryLinkEnricher.java
+++ src/main/java/sonia/scm/jenkins/RepositoryLinkEnricher.java
@@ -16,47 +16,45 @@
 
 package sonia.scm.jenkins;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
+import jakarta.inject.Provider;
+import sonia.scm.api.v2.resources.Enrich;
+import sonia.scm.api.v2.resources.HalAppender;
+import sonia.scm.api.v2.resources.HalEnricher;
+import sonia.scm.api.v2.resources.HalEnricherContext;
 import sonia.scm.api.v2.resources.LinkBuilder;
 import sonia.scm.api.v2.resources.ScmPathInfoStore;
 import sonia.scm.plugin.Extension;
-import sonia.scm.repository.NamespaceAndName;
 import sonia.scm.repository.Repository;
-import sonia.scm.repository.RepositoryManager;
 import sonia.scm.repository.RepositoryPermissions;
-import sonia.scm.web.AbstractRepositoryJsonEnricher;
-
-import jakarta.inject.Provider;
 
 import static sonia.scm.jenkins.JenkinsContext.NAME;
 
 @Extension
-public class RepositoryLinkEnricher extends AbstractRepositoryJsonEnricher {
+@Enrich(Repository.class)
+public class RepositoryLinkEnricher implements HalEnricher {
 
   private final JenkinsContext jenkinsContext;
   private final Provider<ScmPathInfoStore> scmPathInfoStore;
-  private final RepositoryManager repositoryManager;
 
   @Inject
-  public RepositoryLinkEnricher(ObjectMapper objectMapper, JenkinsContext jenkinsContext, Provider<ScmPathInfoStore> scmPathInfoStore, RepositoryManager repositoryManager) {
-    super(objectMapper);
+  public RepositoryLinkEnricher(JenkinsContext jenkinsContext, Provider<ScmPathInfoStore> scmPathInfoStore) {
     this.jenkinsContext = jenkinsContext;
     this.scmPathInfoStore = scmPathInfoStore;
-    this.repositoryManager = repositoryManager;
   }
 
   @Override
-  protected void enrichRepositoryNode(JsonNode repositoryNode, String namespace, String name) {
-    Repository repository = repositoryManager.get(new NamespaceAndName(namespace, name));
-    if (!jenkinsContext.getConfiguration().isDisableRepositoryConfiguration()
-      && RepositoryPermissions.custom(NAME, repository).isPermitted()) {
+  public void enrich(HalEnricherContext halEnricherContext, HalAppender halAppender) {
+    if (jenkinsContext.getConfiguration().isDisableRepositoryConfiguration()) {
+      return;
+    }
+    Repository repository = halEnricherContext.oneRequireByType(Repository.class);
+    if (RepositoryPermissions.custom(NAME, repository).isPermitted()) {
       String linkBuilder = new LinkBuilder(scmPathInfoStore.get().get(), JenkinsConfigurationResource.class)
         .method("getJenkinsConfigForRepository")
-        .parameters(namespace, name)
+        .parameters(repository.getNamespace(), repository.getName())
         .href();
-      this.addLink(repositoryNode, "jenkinsConfig", linkBuilder);
+      halAppender.appendLink("jenkinsConfig", linkBuilder);
     }
   }
 }
